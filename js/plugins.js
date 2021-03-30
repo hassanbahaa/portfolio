8b2f30cb6d1d228aa2e78a1228c8960c19e19a5a
hassanbahaa <66232742+hassanbahaa@users.noreply.github.com>
2021-03-30 13:40:05 +0200

    Add files via upload

---
+++ js/plugins.js
@@ -0,0 +1,116 @@
+/*----------------
+ABOUT SECTION TABS 
+------------------*/
+
+(() =>{
+    const aboutSection      = document.querySelector(".about-section"),
+          tabsContainer     = document.querySelector(".about-tabs");
+
+          tabsContainer.addEventListener("click", (event) =>{
+              /* if event.target contains 'tab item' class and contains 'active' class */
+                if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")  ){
+                    const target = event.target.getAttribute("data-target");
+                    // deactivate existing active 'tab-item'
+                    tabsContainer.querySelector('.active').classList.remove("outer-shadow","active");
+                    // activate new tab-item
+                    event.target.classList.add("active","outer-shadow");
+                    // deactivate existing active 'tab-content'
+                    aboutSection.querySelector(".tab-content.active").classList.remove("active");
+                    // active new 'tab-content'
+                    aboutSection.querySelector(target).classList.add("active");
+                }
+          })
+})();
+
+function bodyScrollingToggle(){
+    document.body.classList.toggle("stop-scrolling");
+}
+
+
+/*----------------
+ABOUT SECTION TABS 
+------------------*/
+
+ /*
+================================================================================
+================================================================================
+================================================================================
+*/ /*
+================================================================================
+================================================================================
+================================================================================
+*/
+
+
+
+/*------------------------
+PORTFOLIO FILTER & POP UP
+--------------------------*/
+
+(() =>{
+     const filterContainer              = document.querySelector(".portfolio-filter"),
+           portfolioItemsContainer      = document.querySelector(".portfolio-items"),
+           portfolioItems               = document.querySelectorAll(".portfolio-item"),
+           popup                        = document.querySelector(".portfolio-popup"),
+           prevBtn                      = popup.querySelector(".pp-prev"),
+           nextBtn                      = popup.querySelector(".pp-next"),
+           closeBtn                     = popup.querySelector(".pp-close"),
+           projectDetailsContainer      = popup.querySelector(".pp-details"),
+           projectDetailsBtn            = popup.querySelector(".pp-project-details-btn");
+           let itemIndex, slideIndex, screenshots;
+
+        //    filter portfolio items
+        filterContainer.addEventListener("click",(event)=>{
+            if(event.target.classList.contains("filter-item") && !event.target.classList.contains("active")){
+                // deactivate existing active 'filter-item'
+                filterContainer.querySelector(".active").classList.remove("outer-shadow","active");
+                // activate new "filter item"
+                event.target.classList.add("active","outer-shadow");
+                const target = event.target.getAttribute("data-target");
+                portfolioItems.forEach((item) => {
+                    if(target === item.getAttribute("data-category") || target === 'all'){
+                        item.classList.remove('hide');
+                        item.classList.add('show');
+                    } else{
+                        item.classList.remove("show");
+                        item.classList.add('hide');
+                    }
+                })
+            }
+            
+            // console.log(item.getAttribute("data-category"));
+        })
+
+        portfolioItemsContainer.addEventListener("click", (event) => {
+            if(event.target.closest(".portfolio-item-inner")){
+                const portfolioItem = event.target.closest(".portfolio-item-inner").parentElement;
+                // get the portfolioItem index
+                itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(portfolioItem);
+                screenshots = portfolioItems[itemIndex].querySelector(".portfolio-item-img img").getAttribute("data-screenshots");
+                // convert screenshots into array
+                screenshots = screenshots.split(",");
+                slideIndex = 0;
+                popupToggle();
+                popupSlideshow();
+            }
+        })
+
+        closeBtn.addEventListener("click", () => {
+            popupToggle();
+        })
+
+        function popupToggle() {
+            popup.classList.toggle("open");
+            bodyScrollingToggle();
+        }
+
+        function popupSlideshow() {
+            const imgSrc = screenshots[slideIndex];
+            const popupImg = popup.querySelector(".pp-img");
+            // activate loader until photos loaded
+            popupImg.src = imgSrc;
+        }
+
+})();
+
+
